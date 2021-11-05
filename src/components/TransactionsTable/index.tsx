53d0d2d3d8315157226e781b323fb8d57e15e89b
Rafael Pivatto <rafael.pivatto@gmail.com>
2021-11-04 23:35:48 -0300

    add http client (axios) + mock (mirage)

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styled";
 
 export function TransactionsTable() {
+    useEffect(() => {
+        api.get('transactions')
+            .then(response => console.log(response.data));
+    }, [])
+
     return (
         <Container>
             <table>
